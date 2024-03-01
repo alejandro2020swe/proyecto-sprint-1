e6fcddb4621bc1cbff7982c66c7c8e9475b647ee
alejandro2020swe <biomealejandro@gmail.com>
2024-02-29 18:49:52 -0600

    casi el final

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import pandas as pd
 import plotly.express as px
 import streamlit as st
 
+# Cargar los datos en un DataFrame de Pandas (reemplaza 'data.csv' con el nombre de tu archivo CSV)
 data = pd.read_csv('corazon.csv')
 
 # Configurar la aplicación Streamlit
@@ -18,17 +19,19 @@ st.write('Los datos contienen información sobre pacientes con enfermedades card
 st.subheader('Datos')
 st.write(data)
 
-# Mostrar un histograma de la edad con más colores
-st.subheader('Histograma de Edad')
-fig_hist = px.histogram(data, x='Age', color='Sex', title='Distribución de Edad por Género')
-st.plotly_chart(fig_hist)
+# Botón para abrir el histograma de edad
+if st.button('Ver Histograma de Edad'):
+    st.subheader('Histograma de Edad')
+    fig_hist = px.histogram(data, x='Age', color='Sex', title='Distribución de Edad por Género')
+    st.plotly_chart(fig_hist)
 
-# Mostrar un gráfico de dispersión de colesterol vs. presión arterial en reposo con más colores
-st.subheader('Gráfico de Dispersión de Colesterol vs. Presión Arterial en Reposo')
-fig_scatter = px.scatter(data, x='RestingBP', y='Cholesterol', color='ChestPainType', title='Colesterol vs. Presión Arterial en Reposo por Tipo de Dolor de Pecho')
-st.plotly_chart(fig_scatter)
+# Botón para abrir el gráfico de dispersión de colesterol vs. presión arterial en reposo
+if st.button('Ver Gráfico de Dispersión de Colesterol vs. Presión Arterial en Reposo'):
+    st.subheader('Gráfico de Dispersión de Colesterol vs. Presión Arterial en Reposo')
+    fig_scatter = px.scatter(data, x='RestingBP', y='Cholesterol', color='ChestPainType', title='Colesterol vs. Presión Arterial en Reposo por Tipo de Dolor de Pecho')
+    st.plotly_chart(fig_scatter)
 
-# Agregar un botón de casilla de verificación para mostrar/ocultar los datos
+# Botón para mostrar/ocultar los datos detallados
 if st.checkbox('Mostrar/Ocultar Datos'):
     st.subheader('Datos Detallados')
     st.write(data)
