de65e4b5ad010d14e1d2940d09c17015c67aa7f4
alejandro2020swe <biomealejandro@gmail.com>
2024-02-29 18:42:16 -0600

    prueba 4

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import pandas as pd
 import plotly.express as px
 import streamlit as st
 
-data = pd.read_csv('corazon.csv')
+data = pd.read_csv('tus_datos.csv')
 
 # Configurar la aplicación Streamlit
 st.title('Análisis de Datos de Enfermedades Cardíacas')
@@ -18,14 +18,14 @@ st.write('Los datos contienen información sobre pacientes con enfermedades card
 st.subheader('Datos')
 st.write(data)
 
-# Mostrar un histograma de la edad
+# Mostrar un histograma de la edad con más colores
 st.subheader('Histograma de Edad')
-fig_hist = px.histogram(data, x='Age', title='Distribución de Edad')
+fig_hist = px.histogram(data, x='Age', color='Sex', title='Distribución de Edad por Género')
 st.plotly_chart(fig_hist)
 
-# Mostrar un gráfico de dispersión de colesterol vs. presión arterial en reposo
+# Mostrar un gráfico de dispersión de colesterol vs. presión arterial en reposo con más colores
 st.subheader('Gráfico de Dispersión de Colesterol vs. Presión Arterial en Reposo')
-fig_scatter = px.scatter(data, x='RestingBP', y='Cholesterol', color='HeartDisease', title='Colesterol vs. Presión Arterial en Reposo')
+fig_scatter = px.scatter(data, x='RestingBP', y='Cholesterol', color='ChestPainType', title='Colesterol vs. Presión Arterial en Reposo por Tipo de Dolor de Pecho')
 st.plotly_chart(fig_scatter)
 
 # Agregar un botón de casilla de verificación para mostrar/ocultar los datos
